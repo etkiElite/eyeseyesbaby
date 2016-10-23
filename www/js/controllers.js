10d803f977e85df60ea29ffa1493f9cf7acd72f5
jcvvdf <jcvvdf@gmail.com>
2016-10-23 13:58:55 -0400

    added demo image

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -54,5 +54,81 @@ function SoundController($ionicPlatform, $timeout, $cordovaNativeAudio) {
        return {x: tap.x, y: tap.y};
     }
 
+    var context;
+    
+    var socket = io();
+    socket.on('liveStream', function(url) {
+      //$('#stream').attr('src', url);
+      $('#stream2').attr('src', url);
+
+      var canvas = document.getElementById('myCanvas');
+        context = canvas.getContext('2d');
+        var imageObj = new Image();
+
+
+        imageObj.onload = function() {
+          context.drawImage(imageObj, 0, 0);
+
+        };
+        imageObj.src = url;
+
+      $('.start').hide();
+    });
+
+    function startStream() {
+      socket.emit('start-stream');
+      $('.start').hide();
+    }
+
+    function getPixelAt(x,y){
+      var cont = context.getImageData(x,y,1,1).data
+      return {"r" : cont[0], "g" : cont[1], "b" : cont[2]};
+    }
+
+    function getVal(x,y){
+      var rgb = getPixelAt(x,y);
+      return rgb2hsv(rgb.r, rgb.g, rgb.b).v;
+    }
+
+    function rgb2hsv () {
+        var rr, gg, bb,
+            r = arguments[0] / 255,
+            g = arguments[1] / 255,
+            b = arguments[2] / 255,
+            h, s,
+            v = Math.max(r, g, b),
+            diff = v - Math.min(r, g, b),
+            diffc = function(c){
+                return (v - c) / 6 / diff + 1 / 2;
+            };
+
+        if (diff == 0) {
+            h = s = 0;
+        } else {
+            s = diff / v;
+            rr = diffc(r);
+            gg = diffc(g);
+            bb = diffc(b);
+
+            if (r === v) {
+                h = bb - gg;
+            }else if (g === v) {
+                h = (1 / 3) + rr - bb;
+            }else if (b === v) {
+                h = (2 / 3) + gg - rr;
+            }
+            if (h < 0) {
+                h += 1;
+            }else if (h > 1) {
+                h -= 1;
+            }
+        }
+        return {
+            h: Math.round(h * 360),
+            s: Math.round(s * 100),
+            v: Math.round(v * 100)
+        };
+    }
+
     return vm;
 }
--- www/js/socket.js
+++ www/js/socket.js
@@ -1,6 +1,6 @@
 app.factory('socket',function(socketFactory){
 	//Create socket and connect to http://chat.socket.io
- 	var myIoSocket = io.connect('http://192.168.43.51:3000/');
+ 	var myIoSocket = io.connect('http://192.168.43.52:3000/');
 
   	mySocket = socketFactory({
     	ioSocket: myIoSocket
